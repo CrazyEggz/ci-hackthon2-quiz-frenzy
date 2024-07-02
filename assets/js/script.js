a041ea28490bf00303a7280c373068f497a6f021
Hannah Eves <heves.work@outlook.com>
2024-07-02 10:31:38 +0000

    added an event listener for when the user inputs their name on the landing page to go to the categories page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,25 @@ document.querySelector('body').addEventListener("mousemove", eyeball);
  */
 function eyeball(event) {
     const eye = document.querySelectorAll(".eye");
-    eye.forEach(function(eye) {
+    eye.forEach(function (eye) {
         let x = (eye.getBoundingClientRect().left + eye.clientWidth / 2);
         let y = (eye.getBoundingClientRect().top + eye.clientHeight / 2);
 
         let radian = Math.atan2(event.pageX - x, event.pageY - y);
         let rotation = (radian * (180 / Math.PI) * -1) + 90;
-        eye.style.transform = "rotate("+rotation+"deg)"
-    })
-}``
+        eye.style.transform = "rotate(" + rotation + "deg)";
+    });
+}
+
+/** After the user enters their name they are taken to the categories page */
+function goToCategoriesPage() {
+
+    document.addEventListener("keydown", function(event) {
+        const button = document.getElementById("name-button");
+        if (button && event.key === "Enter") {
+            window.location.href = 'categories.html';
+        }
+    });
+}
+
+goToCategoriesPage();
